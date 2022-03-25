46369eb817ddbadb3c7daf6187f098c4fddf2772
Thandenw <99381859+NancyThande@users.noreply.github.com>
2022-03-25 15:50:57 +0300

    Betty style compliant

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
-/*
-  add - adds two integers
-  n1: integer 1
-  n2: integer 2
+/**
+ *add - adds two integers
+ *n1: integer 1
+ *n2: integer 2
  
-  Description: give two integers, returns sum
+ *Description: give two integers, returns sum
  
-  Return: sum
+ *Return: sum
  */
 
 int add(int n1, int n2)
